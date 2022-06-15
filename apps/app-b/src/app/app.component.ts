a149ea93946085e726972a24fdad6463c391021b
Altan Stalker <altan@nrwl.io>
2022-06-15 11:55:57 -0400

    add repro

---
+++ apps/app-b/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'unparsed-repro-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'app-b';
+}
